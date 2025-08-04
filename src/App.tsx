cec23e07fb638d817833c183534478817f4fd2ab
olivoemi <olivoemi@gmail.com>
2025-08-04 22:11:12 +0000

    Generated by Spark: Fix all reported errors.

--- src/App.tsx
+++ src/App.tsx
@@ -1942,7 +1942,7 @@ function App() {
                             </div>
                           </div>
                         </div>
-                      </div>
+                    </div>
                   </CardContent>
                 </Card>
               ))
