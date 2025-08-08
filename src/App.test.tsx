d4c8d040d73a26c6217c5f0ca7a8f7e8edaf74cf
olivoemi <olivoemi@gmail.com>
2025-08-08 18:17:32 +0000

    Edited Spark

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 
 // Simple test component to check if React is working
 function TestApp() {
-  return (
+      <p>T
     <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
       <p>Test component is working!</p>
     </div>
-  )
-}
 
-export default TestApp
+
+
